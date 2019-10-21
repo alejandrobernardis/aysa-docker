8246f034829bfd5c86c398d6023a05e22f2d1112
Alejandro M. BERNARDIS <alejandro.m.bernardis@gmail.com>
2019-10-21 15:44:11 -0300

    UPD // +Fixes

--- aysa/commands/deploy.py
+++ aysa/commands/deploy.py
@@ -3,6 +3,20 @@
 # Created: 2019/10/21
 # ~
 
+"""
+    _services = partialmethod(
+        _list,
+        cmd="docker-compose ps --services",
+        filter_line=rx_service
+    )
+
+    _running = partialmethod(
+        _list,
+        cmd="docker-compose ps | grep -P '\s{2,}Up' | awk '{print $1}'",
+        filter_line=rx_container
+    )
+"""
+
 import re
 from pathlib import Path
 from fabric import Connection
@@ -10,31 +24,16 @@ from aysa.commands import Command
 from aysa.docker.registry import Image
 from functools import partial, partialmethod
 
-rx_service = re.compile(r'^[a-z](?:[\w_])+$', re.I)
-rx_image = re.compile(r'^(localhost|[\w\-]+(\.[\w\-]+)+)(?::\d{1,5})?\/', re.I)
-rx_container = re.compile(r'^[a-z](?:[\w_])+\d{1,3}$', re.I)
-
-
-class ConnectionHelper:
-    def __init__(self, connection, auto_close=False):
-        self.connection = connection
-        self.auto_close = auto_close
-
-    def close(self):
-        if self.connection is not None:
-            self.connection.close()
-
-    def __enter__(self, ):
-        if self.connection is None:
-            raise SystemExit('No se estableció la conexión con el servidor.')
-        return self.connection
-
-    def __exit__(self, *exc):
-        if self.auto_close is True:
-            self.close()
+# rx_service = re.compile(r'^[a-z](?:[\w_])+$', re.I)
+# rx_image = re.compile(r'^(localhost|[\w\-]+(\.[\w\-]+)+)(?::\d{1,5})?\/', re.I)
+# rx_container = re.compile(r'^[a-z](?:[\w_])+\d{1,3}$', re.I)
 
 
 class _ConnectionCommand(Command):
+    def execute(self, command, argv=None, global_args=None, **kwargs):
+        super().execute(command, argv, global_args, **kwargs)
+        self.cnx.close()
+
     _stage = None
     _connection_cache = None
 
@@ -46,38 +45,28 @@ class _ConnectionCommand(Command):
                                  'ejecutar despliegues.')
             pkey = Path(env.pop('pkey', None)).expanduser()
             env['connect_kwargs'] = {'key_filename': str(pkey)}
-            self._connection_cache = ConnectionHelper(Connection(**env))
+            self._connection_cache = Connection(**env)
         return self._connection_cache
 
     @property
     def cnx(self):
-        return self._connection_cache.connection
+        return self._connection_cache
+
+    def run(self, command, hide=False, **kwargs):
+        #return self.cnx.run(command, hide=hide, **kwargs)
+        print(command)
 
     def _list(self, cmd, filter_line=None, obj=None):
-        with self._connection() as cx:
-            response = cx.run(cmd, hide=True)
-            for line in response.stdout.splitlines():
-                if filter_line and not filter_line.match(line):
-                    continue
-                yield obj(line) if obj is not None else line
-
-    _images = partialmethod(
-        _list,
-        cmd="docker-compose images | awk '{print $2 \":\" $3}'",
-        filter_line=rx_image,
-        obj=Image
-    )
+        response = self.cnx.run(cmd, hide=True)
+        for line in response.stdout.splitlines():
+            if filter_line and not filter_line.match(line):
+                continue
+            yield obj(line) if obj is not None else line
 
     _services = partialmethod(
         _list,
-        cmd="docker-compose ps --services",
-        filter_line=rx_service
-    )
-
-    _running = partialmethod(
-        _list,
-        cmd="docker-compose ps | grep -P '\s{2,}Up' | awk '{print $1}'",
-        filter_line=rx_container
+        cmd="docker-compose images | awk '{print $1 \";\" $2 \":\" $3}'",
+        filter_line=None
     )
 
 
@@ -92,9 +81,35 @@ class DeployCommand(_ConnectionCommand):
         test    Despliegue en el entorno de `QA/TESTING`.
     """
     def _deploy(self, **kwargs):
+        services = []
+        cnx = self._connection()
         service = kwargs['service']
-        for x in self._running(stage):
-            self.output.write(x)
+
+        # 1. detener los servicios
+        #  >> docker-compose stop
+        self.run('docker-compose stop')
+
+        # 2. buscar los servicios y sus imágenes
+        #  >> docker-compose images | awk '{print $1 ";" $2 ":" $3}'
+        lines = self.run("docker-compose images "
+                         "| awk '{print $1 \";\" $2 \":\" $3}'")
+        # for line in lines.stdout.splitlines():
+        #     container, _, image = line.partition(';')
+        #     services.append((container, image))
+
+        # 3. eliminar los servicios
+        #  >> docker-compose rm [service...]
+        self.run('docker-compose rm {}'
+                 .format(' '.join((x[0] for x in services))))
+
+        # 4. eliminar las imágenes
+        #  >> docker rmi -f $()
+        self.run('docker-compose rmi -f {}'
+                 .format(' '.join((x[1] for x in services))))
+
+        # 5. deplegar
+        #  >> docker-compose up -d
+        self.run('docker-compose up -d')
 
     def deve(self, **kwargs):
         """
